58bd82f38b6ec176215dae287bb51b09b43add33
Swati Mandal <92773616+miss-audiovideo@users.noreply.github.com>
2021-10-23 14:31:10 +0530

    done

--- src/Component/Engineering.js
+++ src/Component/Engineering.js
@@ -4,16 +4,396 @@ import "bulma/css/bulma.css"
 function Engineering() {
   return (
 
-    <section class="hero is-medium is-link">
-    <div class="hero-body">
-      <p class="title">
-        Medium hero
-      </p>
-      <p class="subtitle">
-        Medium subtitle
-      </p>
-    </div>
-  </section>
+    <div>
+    <section className="hero is-large is-black">
+        <div className="hero-body">
+            <div className="container">
+             <div className="columns">
+<div className="column" style={{marginRight:"20px"}}>
+Building a columns layout with Bulma is very simple:
+
+Add a columns container
+Add as many column elements as you want
+Each column will have an equal width, no matter the number of columns.
+Building a columns layout with Bulma is very simple:
+Add a columns container
+Add as many column elements as you want
+Each column will have an equal width, no matter the number of columns.
+</div>
+<div className="column">
+<figure className="image is-2by1">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+</div></div>
+            </div>
+
+        </div>
+    </section>
+                       <div className="container" >
+    <section className="hero is-small is-white has-text-centered" style={{marginLeft:"100px",marginRight:"100px"}}>
+        <div className="hero-body">
+            <p className="title">
+                How Trendius Works
+            </p>
+            <p className="subtitle">
+                Large subtitle
+            </p>
+            <div className="container">
+                <div className="columns is-variable is-4-desktop">
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div className="media-content">
+                                        <p className="title is-4">We get you set up</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                
+                                    <br />
+                                  
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div className="media-content">
+                                        <p className="title is-4">We get you set up</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div className="media-content">
+                                        <p className="title is-4">We get you set up</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-2by1">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div className="media-content">
+                                        <p className="title is-4">We get you set up</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section></div>
+
+    <section className="hero is-medium is-black">
+        <div className="hero-body">
+            <div className="container has-text-centered">
+                <p className="title" style={{color:"red"}}>
+                    The Nisent Difference - We Care
+                </p>
+                <br />
+                <p className="subtitle">
+                    For best asset performance, rotating equipment such as pumps, compressors, fans, turbines, agitators, are expected to run reliably, efficiently and safely meeting process demand – 24×7, round the year.
+
+                    Rotating equipment imparts more than 30% of unplanned downtime in many of process plants and thus, require engineering expertise and experience to achieve production targets and avoid costs due to downtime.
+
+                    We provide you a full suite of engineering services for all your rotating equipment needs.
+                </p>
+
+
+            </div>
+        </div>
+    </section>
+
+
+<div className="container" style={{marginTop:"120px", marginBottom:"140px"}}>
+   <div className="columns">
+<div className="column">
+
+</div>
+<div className="column">
+<h1 style={{color:"#ff000", fontSize:"44px"}}>General Contracting</h1>
+Sample text. Click to select the text box. Click again or double click to start editing the text. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<br/>
+<br/><button className="button is-info">Info</button>
+</div></div></div>
+    <section className="hero is-small is-black has-text-centered">
+        <div className="hero-body">
+            <p className="title" style={{color:"#ff000", fontSize:"44px"}}>
+            What We Offer
+            </p>
+            <p className="subtitle">
+                Large subtitle
+            </p>
+            <div className="container">
+                <div className="columns is-variable is-4-desktop">
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                           
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div >
+                                        <p className="title is-4">Operation and Maintenance</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                <div className="has-text-left" >
+                                We have deep expertise in rotating equipment design and engineering. Our experts provide comprehensive suit of engineering and technical solutions for all your rotating equipment needs.
+                                  <br />
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+
+                </div>
+                <div className="columns is-variable is-4-desktop">
+                <div className="column">
+                        <div >
+                           
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div >
+                                        <p className="title is-4">Operation and Maintenance</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                <div className="has-text-left" >
+                                We have deep expertise in rotating equipment design and engineering. Our experts provide comprehensive suit of engineering and technical solutions for all your rotating equipment needs.
+                                  <br />
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    
+                   
+
+                </div>
+                <div className="columns is-variable is-4-desktop">
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                           
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div >
+                                        <p className="title is-4">Operation and Maintenance</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                <div className="has-text-left" >
+                                We have deep expertise in rotating equipment design and engineering. Our experts provide comprehensive suit of engineering and technical solutions for all your rotating equipment needs.
+                                  <br />
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+
+                </div>
+                <div className="columns is-variable is-4-desktop">
+                <div className="column">
+                        <div >
+                           
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div >
+                                        <p className="title is-4">Operation and Maintenance</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                <div className="has-text-left" >
+                                We have deep expertise in rotating equipment design and engineering. Our experts provide comprehensive suit of engineering and technical solutions for all your rotating equipment needs.
+                                  <br />
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    
+                   
+
+                </div>
+                <div className="columns is-variable is-4-desktop">
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                           
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div >
+                                        <p className="title is-4">Operation and Maintenance</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                <div className="has-text-left" >
+                                We have deep expertise in rotating equipment design and engineering. Our experts provide comprehensive suit of engineering and technical solutions for all your rotating equipment needs.
+                                  <br />
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+
+                </div>
+                <div className="columns is-variable is-4-desktop">
+                <div className="column">
+                        <div >
+                           
+                            <div className="card-content">
+                                <div className="media">
+                                    
+                                    <div >
+                                        <p className="title is-4">Operation and Maintenance</p>
+                                        
+                                    </div>
+                                </div>
+                                <br/>
+
+                                <div className="has-text-left" >
+                                We have deep expertise in rotating equipment design and engineering. Our experts provide comprehensive suit of engineering and technical solutions for all your rotating equipment needs.
+                                  <br />
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div >
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    
+                   
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
 );
 }
 
